f9596328b01ee2903aa079a6cb5d98ca1ea2188e
coffeegrind123 <103264516+coffeegrind123@users.noreply.github.com>
2025-02-13 11:54:41 +0200

    Update xash3d_config_emscripten.mk

--- xash3d_config_emscripten.mk
+++ xash3d_config_emscripten.mk
@@ -1,3 +1,3 @@
 TARGET_ARCH_ABI = emscripten
-CFLAGS_OPT := -Os -funsafe-math-optimizations -fstrict-aliasing -fomit-frame-pointer --llvm-lto 3 -g0 -s INLINING_LIMIT=10
+CFLAGS_OPT := -Os -funsafe-math-optimizations -fstrict-aliasing -fomit-frame-pointer -g0 -s INLINING_LIMIT=1
 LOCAL_CFLAGS += $(CFLAGS_OPT)
